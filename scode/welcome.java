82fa877c42e538746732b1fe8fb41ae286088f25
mpdevops <mukeshsingpg7@gmail.com>
2018-03-14 12:49:47 +0530

    modified java file and release file

--- scode/welcome.java
+++ scode/welcome.java
@@ -4,4 +4,5 @@ trying for pull with merge conflict(edited at windows)
 linux side
 windows side
 adding some functions to master data after code released to ClientA
+after branching to Clientb, adding some functions to ClientB
 
